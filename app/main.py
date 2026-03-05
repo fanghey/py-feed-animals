5014165f63aa8f2f09220a6e591fda2e87b2050f
fanghey <2603Lysenko2603@gmail.com>
2026-03-05 20:06:12 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
-class Animal:
-    def __init__(self, name, appetite, is_hungry=True):
-        self.name = name
-        self.appetite = appetite
-        self.is_hungry = is_hungry
+from typing import List
 
-    def print_name(self):
+
+class Animal:
+    def __init__(self, name: str, appetite: int, is_hungry: bool = True) -> None:
+        self.name: str = name
+        self.appetite: int = appetite
+        self.is_hungry: bool = is_hungry
+
+    def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
 
-    def feed(self):
+    def feed(self) -> int:
         if self.is_hungry:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
@@ -16,23 +19,23 @@ class Animal:
 
 
 class Cat(Animal):
-    def __init__(self, name, is_hungry=True):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, appetite=3, is_hungry=is_hungry)
 
-    def catch_mouse(self):
+    def catch_mouse(self) -> None:
         print("The hunt began!")
 
 
 class Dog(Animal):
-    def __init__(self, name, is_hungry=True):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, appetite=7, is_hungry=is_hungry)
 
-    def bring_slippers(self):
+    def bring_slippers(self) -> None:
         print("The slippers delivered!")
 
 
-def feed_animals(animals):
-    total_food = 0
+def feed_animals(animals: List[Animal]) -> int:
+    total_food: int = 0
     for animal in animals:
         total_food += animal.feed()
     return total_food
